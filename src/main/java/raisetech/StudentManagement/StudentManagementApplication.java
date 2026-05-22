fb77fa12ef0b8f4d56d269c9d349acf73b9d42d5
Ayumi Suzuki <l2304760012@gamil.com>
2026-05-23 03:37:17 +0900

    Mapで学生一覧

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,18 +4,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+
+	private Map<String,String> student = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-@GetMapping("/hello")
-public String hello() {
-	return "Hello,World";
+@GetMapping("/studentInfo")
+public Map<String,String> getStudentInfo() {
+	return student;
+}
+
+@PostMapping ("/studentInfo")
+	public void  setStudentInfo(String name,String age){
+
+		student.put(name,age);
+}
+
+@PostMapping("/studentName")
+	public void updateStudentName(String oldName,String newName){
+		String age = student.get(oldName);
+		student.remove(oldName);
+		student.put(newName,age);
+
 }
 }
